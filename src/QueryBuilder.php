1857487a7147c754d348964a5d5bebffdf799915
bttstn <pietro.battiston@gmail.com>
2021-02-23 16:03:25 +0000

    QueryBuilder and test created

---
+++ src/QueryBuilder.php
@@ -0,0 +1,309 @@
+<?php declare(strict_types=1);
+
+	namespace QueryBuilder;
+
+
+	class QueryBuilder
+	{
+		public $table = '';
+		public $query = '';
+		public $statement = '';
+		public $bindedValues = [];
+		public $dbMethod = '';
+		public $affectedRows;
+
+		public function __construct() 
+		{
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Set the Database Table's Name
+		|--------------------------------------------------------------------------
+		*/
+		public function table(string $tableName):self 
+		{
+			
+			$this->table = $tableName;
+			//emptying values in case have been already used
+			$this->query = '';
+			$this->bindedValues = [];
+
+			return $this;
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Select the Database's columns
+		|--------------------------------------------------------------------------
+		|
+		| It sets the query like this:'SELECT columnName FROM tableName'
+		| 
+		*/
+		public function select(array $selectedColumn):self 
+		{
+			$query = "SELECT @colName FROM @tableName";
+			$selectedColumn = implode(',',$selectedColumn);
+			$placeholdersValues = [
+				"@colName" => $selectedColumn, 
+				"@tableName" => $this->table, 
+			];
+			$this->query = $this->replacePlaceholders($query, $placeholdersValues);
+
+			return $this;
+		}
+
+	    /*
+	    |--------------------------------------------------------------------------
+	    | Build the CREATE query
+	    |--------------------------------------------------------------------------
+	    |
+	    | It sets the query like this:
+	    | INSERT INTO tableName (columnName) VALUES (:columnName)
+	    | or
+	    | INSERT INTO tableName (col1,col2) VALUES (:col1,:col2)
+	    */
+		public function create(array $values):self
+		{
+			/// NEED REFACTOR: LOOK UPDATE METHOD
+			$query = "INSERT INTO @tableName (@colName) VALUES (@bindedCol)";
+			$this->extractBindedValue($values);
+			$columnsName = $this->getColsName($values);
+			$columnsSeparatedByCommma = $this->separateElementsByComma($columnsName);
+			$bindedValues = [];
+			foreach ($columnsName as $name) {
+				$name = ':'. $name;
+				array_push($bindedValues, $name);
+			}
+			$bindedColumnsSeparatedByCommma = $this->separateElementsByComma($bindedValues);
+
+			$placeholdersValues = [
+				"@tableName" => $this->table, 
+				"@colName" => $columnsSeparatedByCommma,
+				"@bindedCol" => $bindedColumnsSeparatedByCommma
+			];
+			$this->query = $this->replacePlaceholders($query, $placeholdersValues);
+			$this->ReturnQuery();
+			return $this;
+		}
+		
+		/*
+	    |--------------------------------------------------------------------------
+	    | Build the UPDATE query
+	    |--------------------------------------------------------------------------
+	    |
+	    | It sets the query like this:
+	    | UPDATE tableName SET columnName=:columnName
+	    | or
+	    | UPDATE tableName SET col1=:col1,col2=:col2
+	    */
+		public function update(array $updateColumns):string
+		{
+
+			// NEED REFACTOR: LOOK UPDATE METHOD
+			$query = "UPDATE @tableName SET @colName";
+			$this->extractBindedValue($updateColumns);
+			$columnsName = $this->getColsName($updateColumns);
+			$bindedValues = [];
+			foreach ($columnsName as $name) {
+				$name = $name . '=:' . $name;
+				array_push($bindedValues, $name);
+			}
+			$columnsSeparatedByCommma = $this->separateElementsByComma($bindedValues);
+			$placeholdersValues = [
+				"@tableName" => $this->table, 
+				"@colName" => $columnsSeparatedByCommma
+			];
+			$query = $this->replacePlaceholders($query, $placeholdersValues);
+			$this->query = $query . $this->query;
+
+			return $this->ReturnQuery();
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Build the DELETE query
+		|--------------------------------------------------------------------------
+		|
+		| It sets the query like this:
+		| "DELETE FROM tableName" plus the already existing query
+		| 
+		|
+		*/
+		public function delete():string
+		{	
+			$query = "DELETE FROM @tableName";
+			$placeholdersValues = [
+				"@tableName" => $this->table
+			];
+			$query = $this->replacePlaceholders($query, $placeholdersValues);
+			$this->query = $query . $this->query;
+
+			return $this->ReturnQuery();
+		}
+
+		/*
+	    |--------------------------------------------------------------------------
+	    | Build the WHERE clause
+	    |--------------------------------------------------------------------------
+	    |
+	    | It sets the query like this:
+	    | Existing Query plus 'WHERE'
+	    | 
+	    | 
+	    */
+		public function where(string $column, string $param, $value):self
+		{
+			$query = " @whereOrAnd @column@param@bindedValue";
+			$bindValue = ':' . $column;
+			$this->bindedValues[$bindValue] = $value;
+			$clause = "WHERE";
+			if (count($this->bindedValues) >= 2) {
+				$clause = "AND";
+			}
+			$placeholdersValues = [
+				"@whereOrAnd" => $clause,
+				"@column" => $column,
+				"@param" => $param,
+				"@bindedValue" => $bindValue
+			];
+			$query = $this->replacePlaceholders($query, $placeholdersValues);
+			$this->query .= $query;
+
+			return $this;
+		}
+
+		public function limit(int $value):self
+		{	$query = ' LIMIT @value';
+			$placeholdersValues = [
+				'@value' => $value
+			];
+			$query = $this->replacePlaceholders($query, $placeholdersValues);
+			$this->query.= $query;
+
+			return $this;
+		}
+
+		public function get():array 
+		{
+			$this->setDbMethod('get');
+			return $this->ReturnQuery();
+		}
+
+		public function first():array
+		{
+			$this->setDbMethod('first');
+			return $this->ReturnQuery();
+		}
+
+
+		/*
+	    |--------------------------------------------------------------------------
+	    | Return the final query
+	    |--------------------------------------------------------------------------
+	    */
+		public function ReturnQuery() 
+		{
+			return $this->query;
+		}
+
+
+		/*
+		|--------------------------------------------------------------------------
+		| Get column's name
+		|--------------------------------------------------------------------------
+		*/
+		
+		private function getColsName(array $cols):array 
+		{
+			$columnsName = array_keys($cols);
+			return $columnsName;
+		}
+
+		/*
+	    |--------------------------------------------------------------------------
+	    | Set an array containing the column's name prefixed with ':' its own value. 
+	    | The array will be used to bind the values before the query execution
+	    |--------------------------------------------------------------------------
+	    |
+	    | It sets an array ($this->bindedValues) containing columns name as Key with 	 | the related value.
+	    |
+	    | 
+	    */
+		private function extractBindedValue(array $columns):void 
+		{
+			$loopIndex = 0;
+			$values = '';
+			foreach ($columns as $key => $value) {
+					$loopIndex++;
+					$columnName = $key;
+					$bindValue = ':' . $columnName;
+					$this->bindedValues[$bindValue] = $value;
+			}
+		}
+
+		private function bindValues() 
+		{
+			foreach ($this->bindedValues as $key => $value) {
+				$this->db->bind($key, $value);
+			}
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Separate array's elements by a comma
+		|--------------------------------------------------------------------------
+		|
+		| Given an array it returns a string with all the elements separated by a comma
+		| Input: [3,4,5]
+		| Output: '3,4,5'
+		|
+		*/
+		private function separateElementsByComma(array $values):string 
+		{
+			// If the numbers of elements inside the $columns array is still greater than $loopIndex, a comma will separate the columns. Otherwise it will be set as empty. (title=:title,name=:name,etc.)
+			$loopIndex = 0;
+			$valuesSeparatedByCommma = '';
+			foreach ($values as $value) {
+				$loopIndex++;
+				$separator = count($values) > $loopIndex ? ',' : '';
+				$valuesSeparatedByCommma .= $value . $separator;
+			}
+			return $valuesSeparatedByCommma;
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Set $this->dbMethod to the given value. It will be passed to Database Class
+		|--------------------------------------------------------------------------
+		|
+		| The Database Class can know which method must call.
+		| Examples:
+		| 'get' will call the get() method of Database Class.
+		| 'first' will call the first() method of Database Class.
+		| 'execute' will call the execute() method of Database Class.
+		*/
+		private function setDbMethod(string $method):void
+		{
+			$this->dbMethod = $method;
+		}
+
+		/*
+		|--------------------------------------------------------------------------
+		| Replace placeholders inside the given string with values of an array
+		|--------------------------------------------------------------------------
+		|
+		| Input: "string with @placeholder"
+		| Output: "string with value"
+		| 
+		|
+		*/
+
+		private function replacePlaceholders(string $query, array $values):string
+		{
+			$query = strtr($query, $values);
+			
+			return $query;
+		}
+
+	}
---
+++ tests/QueryBuilderTest.php
@@ -0,0 +1,172 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+use QueryBuilder\QueryBuilder;
+
+
+
+
+final class QueryBuilderTest extends TestCase
+{
+    public function setUp():void {
+        $this->QB = new QueryBuilder;
+        $this->tableName = 'myTable';
+    }
+
+    public function test_a_table_can_be_selected(): void
+    {
+        
+         $this->QB->table($this->tableName);
+
+         $this->assertEquals($this->QB->table, $this->tableName);
+    }
+
+    public function test_CREATE_query_can_be_built(): void
+    {
+        $this->QB->table = $this->tableName;
+        $values = [
+            'title' => 'new post'
+        ];
+        $bindedTitle = ':title';
+        $this->QB->create($values);
+        $expectedQuery = "INSERT INTO " .$this->tableName . " (title) VALUES (:title)";
+        $this->assertEquals($this->QB->query, $expectedQuery);
+        $this->assertTrue(is_array($this->QB->bindedValues));
+        $this->assertArrayHasKey($bindedTitle, $this->QB->bindedValues);
+        $this->assertTrue(empty($this->QB->dbMethod));
+
+    }
+
+    public function test_CREATE_query_can_be_built_with_multiple_params(): void
+    {
+        $this->QB->table = $this->tableName;
+        $values = [
+            'title' => 'new post',
+            'id' => 32
+        ];
+        $bindedTitle = ':title';
+        $bindedId = ':id';
+        $this->QB->create($values);
+        $expectedQuery = "INSERT INTO " .$this->tableName . " (title,id) VALUES (:title,:id)";
+        $this->assertEquals($this->QB->query, $expectedQuery);
+        $this->assertTrue(is_array($this->QB->bindedValues));
+        $this->assertArrayHasKey($bindedTitle, $this->QB->bindedValues);
+        $this->assertArrayHasKey($bindedId, $this->QB->bindedValues);
+        $this->assertTrue(empty($this->QB->dbMethod));
+
+
+    }
+
+    public function test_UPDATE_query_can_be_built(): void
+    {
+         $this->QB->table = $this->tableName;
+         
+         $newTitle  = 'myTitle';
+         $this->QB->update([
+            'title' => $newTitle
+         ]);
+         $bindedTitle = ':title';
+         $expectedQuery =  'UPDATE ' .$this->tableName . ' SET title=:title';
+         $this->assertEquals($this->QB->query, $expectedQuery);
+         $this->assertTrue(is_array($this->QB->bindedValues));
+         $this->assertArrayHasKey($bindedTitle, $this->QB->bindedValues);
+         $this->assertTrue(empty($this->QB->dbMethod));
+
+    }
+
+    public function test_UPDATE_query_must_contains_multiple_binded_elements_divided_by_a_comma(): void
+    {
+
+         $this->QB->table = $this->tableName;
+         
+
+         $newTitle  = 'myTitle';
+         $this->QB->update([
+            'title' => $newTitle,
+            'id' => 32
+         ]);
+         $bindedTitle = ':title';
+         $bindedId = ':id';
+         $expectedQuery = 'UPDATE ' . $this->tableName . ' SET title=:title,id=:id';
+         $this->assertEquals($this->QB->query, $expectedQuery);
+         $this->assertArrayHasKey($bindedTitle, $this->QB->bindedValues);
+         $this->assertArrayHasKey($bindedId, $this->QB->bindedValues);
+         $this->assertTrue(empty($this->QB->dbMethod));
+
+    }
+
+    public function test_DELETE_query_can_be_built(): void
+    {
+         $this->QB->table = $this->tableName;
+         
+         $this->QB->delete();
+         $expectedQuery = 'DELETE FROM ' . $this->tableName;
+         $this->assertEquals($this->QB->query, $expectedQuery);
+         $this->assertTrue(empty($this->QB->dbMethod));
+
+    }
+
+    public function test_WHERE_clause_can_be_built(): void
+    {
+         $this->QB->table = $this->tableName;
+         $this->QB->where('id', '=', 32);
+         $expectedQuery = ' WHERE id=:id';
+         $this->assertEquals($this->QB->query, $expectedQuery);
+    }
+
+    public function test_multiple_WHERE_clauses_must_be_separated_by_AND(): void
+    {
+         $this->QB->table = $this->tableName;
+         $this->QB->where('id', '=', 32);
+         $this->QB->where('title', '=', 'title');
+         $expectedQuery = ' WHERE id=:id AND title=:title';
+         $this->assertEquals($this->QB->query, $expectedQuery);
+    }
+
+    public function test_column_can_be_selected(): void {
+
+         $this->QB->table = $this->tableName;
+         $this->QB->select(['title']);
+         $expectedQuery = 'SELECT title FROM ' . $this->tableName;
+         $this->assertEquals($this->QB->query, $expectedQuery);
+    }
+
+    public function test_selecting_multiple_column_must_return_col_separated_by_comma(): void
+    {
+         $this->QB->table = $this->tableName;
+         $this->QB->select(['title, id']);
+         $expectedQuery = 'SELECT title, id FROM ' . $this->tableName;
+         $this->assertEquals($this->QB->query, $expectedQuery);
+    }
+
+    public function test_LIMIT_clause_can_be_built(): void
+    {
+         $this->QB->limit(2);
+         $expectedQuery = ' LIMIT 2';
+         $this->assertEquals($this->QB->query, $expectedQuery);
+    }
+
+    // public function test_calling_Get_Method_must_set_the_var_dbMethod(): void {
+    //      $this->QB = new DB($this->dbMock);
+    //      $this->QB->get();
+    //      $this->assertEquals($this->QB->dbMethod, 'get');
+    // }
+
+    // public function test_calling_First_Method_must_set_the_var_dbMethod(): void {
+    //      $this->QB = new DB($this->dbMock);
+    //      $this->QB->first();
+    //      $this->assertEquals($this->QB->dbMethod, 'first');
+    // }
+    // public function test_replacePlaceholders(): void {
+    //      $this->QB = new DB($this->dbMock);
+    //      $query = "Hello @name today is @day";
+    //      $array = [
+    //         "@name" => 'pietro',
+    //         "@day" => 'saturday'
+    //      ];
+    //      $expectedQuery = "Hello pietro today is saturday";
+    //      $replace = $this->QB->replacePlaceholders($query, $array);
+    //      $this->assertEquals($replace, $expectedQuery);
+    // }
+
+}
